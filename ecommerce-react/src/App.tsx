11210c0ff2446befc213bd617240c569211902c9
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-11-01 00:50:20 +0200

    some folder created

--- ecommerce-react/src/App.tsx
+++ ecommerce-react/src/App.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import './App.css';
 import { Button } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Navbar from './customer/components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="">
-      <h1 className='font-bold text-5xl'>home page</h1>
-      <Button variant='contained'>test</Button>
-      <ShoppingCartIcon/>
+     <Navbar/>
     </div>
   );
 }
---
+++ ecommerce-react/src/customer/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Box, IconButton } from "@mui/material";
+import React from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+  return (
+    <div>
+      <Box>
+        <div>
+          <div>
+            <div>
+<IconButton>
+    <MenuIcon/>
+</IconButton>
+<h1 className="logo cursor-pointer text-lg md:text-2xl text-[#00927a]">XAG</h1>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </div>
+  );
+}
+
+export default Navbar;
