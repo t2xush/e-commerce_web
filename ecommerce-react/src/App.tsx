d54e324788dc4e6d105a844578098edd5b76b344
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-11-01 00:33:18 +0200

    mui tailwindcss install and test

--- ecommerce-react/src/App.tsx
+++ ecommerce-react/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Button } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1 className='font-bold text-5xl'>home page</h1>
+      <Button variant='contained'>test</Button>
+      <ShoppingCartIcon/>
     </div>
   );
 }
