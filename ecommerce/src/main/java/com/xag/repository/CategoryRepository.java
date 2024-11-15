98b8223711dc5cc6118e3b24612690d52606e948
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-11-15 17:48:55 +0200

    product service(define product properties and method)

---
+++ ecommerce/src/main/java/com/xag/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.xag.repository;
+
+import com.xag.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Category findByCategoryId(String categoryId);
+}
