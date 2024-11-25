eb2c5961ea25f66e966bc760dbf97a2b000e78b3
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-11-25 14:51:06 +0200

    developed backend functionalities for coupon and apis

--- ecommerce/src/main/java/com/xag/service/CouponService.java
+++ ecommerce/src/main/java/com/xag/service/CouponService.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface CouponService {
     Cart applyCoupon(String code, double orderValue, User user) throws Exception;
-    Cart removeCoupon(String code,User user);
-    Coupon findCouponById(Long id);
+    Cart removeCoupon(String code,User user) throws Exception;
+    Coupon findCouponById(Long id) throws Exception;
     Coupon createCoupon(Coupon coupon);
     List<Coupon> findAllCoupons();
-    void deleteCoupon(Long id);
+    void deleteCoupon(Long id) throws Exception;
 }
--- ecommerce/src/main/java/com/xag/service/impl/CouponServiceImpl.java
+++ ecommerce/src/main/java/com/xag/service/impl/CouponServiceImpl.java
@@ -5,10 +5,13 @@ import com.xag.model.Coupon;
 import com.xag.model.User;
 import com.xag.repository.CartRepository;
 import com.xag.repository.CouponRepository;
+import com.xag.repository.UserRepository;
 import com.xag.service.CouponService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -16,6 +19,7 @@ import java.util.List;
 public class CouponServiceImpl implements CouponService {
     private final CouponRepository couponRepository;
     private final CartRepository cartRepository;
+    private final UserRepository userRepository;
 
     @Override
     public Cart applyCoupon(String code, double orderValue, User user) throws Exception {
@@ -30,33 +34,57 @@ public class CouponServiceImpl implements CouponService {
         if (orderValue<coupon.getMinimumOrderValue()){
             throw new Exception(" valid for minimum order value"+coupon.getMinimumOrderValue());
         }
+        if(coupon.isActive()&&
+                LocalDate.now().isAfter(coupon.getValidityStartDate())
+              &&LocalDate.now().isBefore(coupon.getValidityEndDate())){
+            user.getUsedCoupons().add(coupon);
+            userRepository.save(user);
 
-
-        return null;
+            double discountedPrice=(cart.getTotalSellingPrice()*coupon.getDiscountPercentage())/100;
+            cart.setTotalSellingPrice(cart.getTotalSellingPrice()-discountedPrice);
+            cart.setCouponCode(code);
+            cartRepository.save(cart);
+            return cart;
+        }
+        throw new Exception("coupon not valid");
     }
 
     @Override
-    public Cart removeCoupon(String code, User user) {
-        return null;
+    public Cart removeCoupon(String code, User user) throws Exception {
+        Coupon coupon=couponRepository.findByCode(code);
+        if (coupon==null){
+            throw new Exception("coupon not found");
+        }
+        Cart cart=cartRepository.findByUserId(user.getId());
+
+        double discountedPrice=(cart.getTotalSellingPrice()*coupon.getDiscountPercentage())/100;
+        cart.setTotalSellingPrice(cart.getTotalSellingPrice()-discountedPrice);
+        cart.setCouponCode(null);
+        return cartRepository.save(cart);
     }
 
     @Override
-    public Coupon findCouponById(Long id) {
-        return null;
+    public Coupon findCouponById(Long id) throws Exception {
+        return couponRepository.findById(id).orElseThrow(()->
+                        new Exception("coupon not found"));
     }
 
     @Override
+    @PreAuthorize("hasRole ('ADMIN')")
     public Coupon createCoupon(Coupon coupon) {
-        return null;
+        return couponRepository.save(coupon);
     }
 
     @Override
     public List<Coupon> findAllCoupons() {
-        return null;
+        return couponRepository.findAll();
     }
 
     @Override
-    public void deleteCoupon(Long id) {
+    @PreAuthorize("hasRole ('ADMIN')")
+    public void deleteCoupon(Long id) throws Exception {
+        findCouponById(id);
+        couponRepository.deleteById(id);
 
     }
 }
