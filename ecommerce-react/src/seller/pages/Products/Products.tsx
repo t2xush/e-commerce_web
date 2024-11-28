3062dd97a2c3bba038e37fcc1bf677ecd5a75aad
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-11-28 18:44:55 +0200

    initial seller products page ui

--- ecommerce-react/src/seller/pages/Products/Products.tsx
+++ ecommerce-react/src/seller/pages/Products/Products.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import ProductTable from './ProductTable'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+        <h1 className='font-bold mb-5 text-xl'>All Product</h1>
+        <ProductTable/>
+    </div>
   )
 }
 
