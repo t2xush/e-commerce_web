7b0a2eed4f8421c0ef815ef470d9504c34540289
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-12-09 22:43:06 +0200

    proceed to payment page success after order created

--- ecommerce/src/main/java/com/xag/service/impl/PaymentServiceImpl.java
+++ ecommerce/src/main/java/com/xag/service/impl/PaymentServiceImpl.java
@@ -115,6 +115,6 @@ public class PaymentServiceImpl implements PaymentService {
 
         Session session=Session.create(params);
 
-        return session.getCancelUrl();
+        return session.getUrl();
     }
 }
